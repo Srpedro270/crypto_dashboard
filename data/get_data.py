ba70744fc5e62980f738b2785d93dd275df3ca53
Gustavo pedro <gustavo.pedro.dev27@outlook.com>
2026-04-19 02:48:11 +0300

    alterando data

--- data/get_data.py
+++ data/get_data.py
@@ -1,5 +1,9 @@
 import requests
 import pandas as pd
+import streamlit as st
+
+
+@st.cache_data(ttl=300)
 
 def get_crypto_data():
     url = "https://api.coingecko.com/api/v3/coins/markets"
@@ -11,12 +15,29 @@ def get_crypto_data():
         "page": 1
     }
 
-    resposta = requests.get(url, params=params)
-    dados = resposta.json()
+    try:
+        resposta = requests.get(url, params=params, timeout=10)
+        resposta.raise_for_status()
 
-    df = pd.DataFrame(dados)
+        dados = resposta.json()
 
-    return df
+        if not isinstance(dados, list):
+            return pd.DataFrame()
+
+        df = pd.DataFrame(dados)
+
+        # garante que colunas existem
+        colunas = ["name", "current_price", "price_change_percentage_24h"]
+
+        for col in colunas:
+            if col not in df.columns:
+                return pd.DataFrame()
+
+        return df[colunas]
+
+    except Exception as e:
+        print("Erro API:", e)
+        return pd.DataFrame()
 
 
 # teste rápido (rodar esse arquivo sozinho)
